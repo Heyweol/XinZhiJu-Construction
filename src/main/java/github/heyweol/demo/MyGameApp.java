76ca57aa97fdf6be3fbdfc144aace4e685b94981
Haiyue Liu <112049812+Heyweol@users.noreply.github.com>
2024-10-17 19:16:06 -0500

    add tutorial

--- src/main/java/github/heyweol/demo/MyGameApp.java
+++ src/main/java/github/heyweol/demo/MyGameApp.java
@@ -1,6 +1,5 @@
 package github.heyweol.demo;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,24 +12,22 @@ import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
 
-import com.almasb.fxgl.app.FXGLApplication;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.dsl.FXGL;
+import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
+import static com.almasb.fxgl.dsl.FXGL.getGameTimer;
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
+import static com.almasb.fxgl.dsl.FXGL.getInput;
+import static com.almasb.fxgl.dsl.FXGL.getWorldProperties;
+import static com.almasb.fxgl.dsl.FXGL.spawn;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.SpawnData;
-import com.almasb.fxgl.entity.components.BoundingBoxComponent;
 import com.almasb.fxgl.input.UserAction;
-import com.almasb.fxgl.app.GameSettings;
 
 import github.heyweol.demo.components.GridVisualizerComponent;
 import github.heyweol.demo.components.InteractiveItemComponent;
-import github.heyweol.demo.components.ZIndexComponent;
-import github.heyweol.demo.ui.ItemBar;
-import github.heyweol.demo.ui.MainGameScene;
-import github.heyweol.demo.ui.MaterialSummaryWindow;
-import github.heyweol.demo.ui.RadialMenu;
+import github.heyweol.demo.ui.*;
 import github.heyweol.demo.utils.ResourceManager;
 import github.heyweol.demo.utils.SceneManager;
 import javafx.geometry.Point2D;
@@ -40,21 +37,16 @@ import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Transform;
 import javafx.util.Duration;
-
-import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class MyGameApp extends GameApplication {
   
@@ -99,7 +91,8 @@ public class MyGameApp extends GameApplication {
   private RadialMenu radialMenu;
   private Supplier<Map<String,Integer>> materailSupplier = this::getMaterialList;
   private Supplier<File> screenshotSupplier = this::takeCustomScreenshot;
-  
+  private TutorialWindow tutorialWindow;
+
   
   @Override
   protected void initSettings(GameSettings settings) {
@@ -242,6 +235,11 @@ public class MyGameApp extends GameApplication {
               mousePos.getX(), mousePos.getY(),
               gridPos.getX(), gridPos.getY()));
     });
+    
+    tutorialWindow = new TutorialWindow();
+    FXGL.addUINode(tutorialWindow);
+    
+    showTutorial();
   }
   
   @Override
@@ -318,6 +316,10 @@ public class MyGameApp extends GameApplication {
         togglePauseMenu();
       }
     }, KeyCode.ESCAPE);
+  }
+  
+  private void showTutorial() {
+    tutorialWindow.show();
   }
   
   @Override
@@ -506,6 +508,13 @@ public class MyGameApp extends GameApplication {
     pauseMenu.setTranslateX(FXGL.getAppWidth() / 2 - 100);
     pauseMenu.setTranslateY(FXGL.getAppHeight() / 2 - 50);
     
+    // Tutorial Button
+    Button tutorialButton = new Button("Show Tutorial");
+    tutorialButton.setOnAction(e -> {
+      hidePauseMenu();
+      showTutorial();
+    });
+    
     // Dev Mode Toggle
     CheckBox devModeCheckBox = new CheckBox("Enable Dev Mode");
     devModeCheckBox.setSelected(isDevMode);
@@ -523,7 +532,7 @@ public class MyGameApp extends GameApplication {
     Button closeButton = new Button("Close");
     closeButton.setOnAction(e -> hidePauseMenu());
     
-    pauseMenu.getChildren().addAll(devModeCheckBox, closeButton);
+    pauseMenu.getChildren().addAll(tutorialButton, devModeCheckBox, closeButton);
     FXGL.addUINode(pauseMenu);
   }
   
